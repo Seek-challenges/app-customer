4d202f7bc8d12518ebb61a8fc9946387ae29d87f
Carlos Arturo Diaz <diazartiagacarlosarturo@gmail.com>
2025-05-19 17:06:10 -0500

    Update AppCustomerRequestDTO to enforce non-null birthDate validation

--- src/main/java/pe/seek/app/customer/infrastructure/adapter/controller/dto/AppCustomerRequestDTO.java
+++ src/main/java/pe/seek/app/customer/infrastructure/adapter/controller/dto/AppCustomerRequestDTO.java
@@ -1,6 +1,7 @@
 package pe.seek.app.customer.infrastructure.adapter.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
@@ -13,7 +14,7 @@ public record AppCustomerRequestDTO(
         String phone,
         @NotBlank
         String age,
-        @NotBlank
+        @NotNull
         LocalDate birthDate,
         @NotBlank
         String password
